99fcdb1c2e90900be6579efac717d0f09a088c1f
CedricDeVijt <46058254+CedricDeVijt@users.noreply.github.com>
2026-04-28 14:26:27 +0200

    configure database connection settings and add example environment variables

--- backend/src/db/__init__.py
+++ backend/src/db/__init__.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
+from src.core.config import settings
 from src.db.base import Base
 
-DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
