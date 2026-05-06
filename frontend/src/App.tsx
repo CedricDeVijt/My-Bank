f90818da19c7f3386e648aee7270080507f024a3
CedricDeVijt <46058254+CedricDeVijt@users.noreply.github.com>
2026-05-06 17:49:45 +0200

    style: format JSX in App component for improved readability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from "react-router-dom";
 
-import { LoginPage } from "./pages/LoginPage.tsx";
-import { HomePage } from "./pages/HomePage.tsx";
-import { NavBar } from "./components/NavBar.tsx";
+import {LoginPage} from "./pages/LoginPage.tsx";
+import {HomePage} from "./pages/HomePage.tsx";
+import {NavBar} from "./components/NavBar.tsx";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <Router>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
